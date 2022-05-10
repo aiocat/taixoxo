4514858a071797e325796d39b6a116c3c848f3f7
aiocat <aiocat@protonmail.com>
2022-05-10 22:14:38 +0300

    add simple pixel utility

--- src/screen.rs
+++ src/screen.rs
@@ -14,11 +14,27 @@
 // 
 // You should have received a copy of the GNU General Public License
 // along with taixoxo.  If not, see <http://www.gnu.org/licenses/>.
-mod screen;
 
-fn main() {
-    let screen_handle = screen::get_screen();
-    let (r, g, b) = screen::get_pixel(screen_handle, 27, 155);
-    println!("RGB({}, {}, {})", r, g, b);
-    screen::free_screen(screen_handle);
+use winapi::um::wingdi::{GetPixel, GetRValue, GetGValue, GetBValue};
+use winapi::um::winuser::{ReleaseDC, GetDC};
+use winapi::shared::windef::HDC;
+
+use std::ptr;
+
+pub type SCREEN = HDC; // screen alias for HDC
+
+// get screen function
+pub fn get_screen() -> SCREEN {
+    unsafe { GetDC(ptr::null_mut()) }
+}
+
+// free screen function for memory safety
+pub fn free_screen(screen: SCREEN) -> bool {
+    (unsafe { ReleaseDC(ptr::null_mut(), screen) }) as u8 == 1
+}
+
+// get pixel color from x and y position
+pub fn get_pixel(screen: SCREEN, x: i32, y: i32) -> (u8, u8, u8) {
+    let color = unsafe { GetPixel(screen, x, y) };
+    (GetRValue(color), GetGValue(color), GetBValue(color))
 }
